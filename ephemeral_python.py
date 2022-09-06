522912d82f586d3f0f18fb1661102d4415b100e8
Simon Walker <s.r.walker101@googlemail.com>
2022-09-06 13:02:40 +0100

    feat: changes

--- ephemeral_python.py
+++ ephemeral_python.py
@@ -3,32 +3,84 @@
 
 import argparse
 import copy
+import hashlib
 import os
+import shutil
 import subprocess as sp
-import tempfile
+import sys
+import threading
+import time
 import venv
+from dataclasses import dataclass
 from pathlib import Path
 from types import SimpleNamespace
 from typing import List
 
+from xdg import xdg_data_home
+
+
+class Spinner:
+    """
+    https://stackoverflow.com/a/39504463
+    """
+
+    busy = False
+    delay = 0.1
+
+    @staticmethod
+    def spinning_cursor():
+        while 1:
+            for cursor in "|/-\\":
+                yield cursor
+
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay):
+            self.delay = delay
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write("\b")
+            sys.stdout.flush()
+
+    def __enter__(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def __exit__(self, exception, value, tb):
+        self.busy = False
+        time.sleep(self.delay)
+        if exception is not None:
+            return False
+
+
+@dataclass
+class Args:
+    package: List[str]
+    recreate: bool
+    update: bool
+
 
 class EnvBuilder(venv.EnvBuilder):
-    def __init__(self, env_dir: Path, packages: List[str]) -> None:
+    def __init__(
+        self, env_dir: Path, packages: List[str], update: bool, recreate: bool
+    ) -> None:
         super().__init__(with_pip=True)
+
         self.env_dir = env_dir
-        self.additional_packages = packages
+        self.packages = packages
+        self.update = update
+        self.recreate = recreate
 
     def post_setup(self, context: SimpleNamespace) -> None:
-        print("Adding packages")
-        packages = [
-            "IPython",
-        ] + self.additional_packages
-
-        self._install_packages(packages)
+        self._install_packages()
 
         return super().post_setup(context)
 
-    def _install_packages(self, packages: List[str]) -> None:
+    def _install_packages(self) -> None:
         interpreter_path = self.env_dir / "bin" / "python"
         if not interpreter_path.is_file():
             raise RuntimeError(f"cannot find python interpreter at {interpreter_path}")
@@ -38,8 +90,12 @@ class EnvBuilder(venv.EnvBuilder):
             "-m",
             "pip",
             "install",
-        ] + packages
-        sp.run(cmd, check=True)
+        ]
+        if self.update:
+            cmd.append("-U")
+
+        cmd.extend(self.packages)
+        sp.run(cmd, check=True, stdout=sp.PIPE, stderr=sp.PIPE)
 
     def run_ipython(self) -> None:
         ipython_path = self.env_dir / "bin" / "ipython"
@@ -52,16 +108,47 @@ class EnvBuilder(venv.EnvBuilder):
         os.execle(str(ipython_path), str(ipython_path), env)
 
     def create(self) -> None:
-        super().create(self.env_dir)
+        if self.env_dir.is_dir() and not self.recreate:
+            return
+
+        with Spinner():
+            if self.recreate:
+                shutil.rmtree(self.env_dir)
+
+            super().create(self.env_dir)
+
+
+def compute_package_list_hash(package_list: List[str]) -> str:
+    hasher = hashlib.md5()
+
+    for package in package_list:
+        hasher.update(package.encode("utf8"))
+
+    return hasher.hexdigest()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("package", nargs="*")
+    parser.add_argument("-r", "--recreate", action="store_true", default=False)
+    parser.add_argument("-u", "--update", action="store_true", default=False)
     args = parser.parse_args()
 
-    dest = Path(tempfile.mkdtemp(prefix="ephemeral-python-"))
-    print(f"Creating venv {dest}")
-    builder = EnvBuilder(dest, args.package)
+    all_packages = sorted(
+        list(set(["ipython"] + [every.lower() for every in args.package]))
+    )
+
+    # re-usable environments
+    package_list_hash = compute_package_list_hash(all_packages)
+
+    dest = (
+        Path(xdg_data_home())
+        / "ephemeral-python-venvs"
+        / f"ephemeral-python-{package_list_hash}"
+    )
+
+    builder = EnvBuilder(
+        env_dir=dest, packages=all_packages, recreate=args.recreate, update=args.update
+    )
     builder.create()
     builder.run_ipython()
